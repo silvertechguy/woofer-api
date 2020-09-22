2be3aea7a6175c6916112a8809d9e91450196992
Ali Fadda <silvertechguy@gmail.com>
2020-09-23 01:51:51 +0200

    fix env config

--- config/db.js
+++ config/db.js
@@ -2,20 +2,20 @@ const { Pool } = require("pg");
 
 let config = null;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
   config = {
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     database: process.env.DATABASE_NAME,
-  };
-} else {
-  config = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
   };
 }
 
--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,1 @@
-let config;
-
-if (process.env.NODE_ENV === "development") {
-  config = null;
-} else {
-  config = process.env.REDIS_URL;
-}
-
-module.exports = require("redis").createClient(config);
+module.exports = require("redis").createClient(process.env.REDIS_URL);
